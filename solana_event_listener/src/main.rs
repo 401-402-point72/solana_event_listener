5951d4b30dd118bcbd653bb80fb30ce00cf56e32
Ryan Carnes <123479367+Ryan-Carnes-01@users.noreply.github.com>
2024-04-02 23:47:46 -0400

    Merge JSON object updates from Ryan-Branch (#12)
    * refactored functions and printing, added minor debugging statements, cleaned up comments and readme
    
    * Switched from devnet to mainnet, readme updated
    
    * Fixed transaction retrieval
    
    * Updated JSON organization structure
    
    * modified json structure
    
    ---------
    
    Co-authored-by: Ryan-Carnes-01 <ryancarnes2001@gmail.com>

--- solana_event_listener/src/main.rs
+++ solana_event_listener/src/main.rs
@@ -3,7 +3,6 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-
 use solana_client::rpc_client::RpcClient;
 use solana_client::rpc_config::RpcBlockConfig;
 use solana_sdk::commitment_config::CommitmentConfig;
@@ -17,62 +16,75 @@ use solana_transaction_status::{
     UiTransactionEncoding,
     TransactionDetails};
 use chrono::{LocalResult, TimeZone, Utc, DateTime};
-use serde_json::json;
+use serde_json::{json,Value};
+use std::fs::File;
+use std::io::Write;
 
 const MAX_ITER : i32 = 10; //number of slots to get and try to retrieve a block from before terminating
-const MAX_RETRIES : i32 = 5; //number of retries to retrieve block from the current slot before timing out
+const MAX_RETRIES : i32 = 3; //number of retries to retrieve block from the current slot before timing out
 
-fn parse_transactions(transactions: Vec<EncodedTransactionWithStatusMeta>) { //parses block transaction data vector
-    println!("Total number of transactions: {}", transactions.len());
+fn parse_transactions(transactions: Vec<EncodedTransactionWithStatusMeta>) -> Value { //parses block transaction data vector
+    let total_transactions = transactions.len();
+    let mut transaction_info_vec: Vec<Value> = Vec::new();
+    //gather all transactions into JSONs
     for transaction in transactions{
-        //jsonify transaction info
-        let transaction_info = json!({
+        let transaction_info_segment = json!({
             "transaction": transaction.transaction,
-            "meta": transaction.meta,
+            "fee": transaction.meta.clone().unwrap().fee,
+            "compute_units_consumed": transaction.meta.clone().unwrap().compute_units_consumed,
             "version": transaction.version,
         });
-
-        //print, (store in database later)
-        //println!("Parsed Transaction: {}", transaction_info);
+        transaction_info_vec.push(transaction_info_segment);
     }
+    //combine into final JSON
+    let transaction_info = json!({
+        "total_transactions": total_transactions,
+        "all_transactions_info": json!(transaction_info_vec)
+    });
+    return transaction_info;
 }
 
-fn parse_rewards(rewards: Vec<Reward>) { //parses block reward data vector
-    println!("Total number of rewards: {}", rewards.len());
+fn parse_rewards(rewards: Vec<Reward>) -> Value { //parses block reward data vector
+    let total_rewards = rewards.len();
+    let mut reward_info_vec: Vec<Value> = Vec::new();
     for reward in rewards {
-        //jsonify reward info
-        let reward_info = json!({
+        //gather all rewards into JSONs
+        let reward_info_segment = json!({
             "pubkey": reward.pubkey,
             "lamports": reward.lamports,
             "post balance": reward.post_balance,
             "reward type": reward.reward_type,
             "commission": reward.commission,
         });
-
-        //print, (store in database later)
-        println!("Parsed Reward: {}", reward_info);
+        reward_info_vec.push(reward_info_segment);
     }
+    //combine into final JSON
+    let reward_info = json!({
+        "total_rewards": total_rewards,
+        "all_rewards_info": json!(reward_info_vec)
+    });
+    return reward_info;
 }
 
 fn parse_block(encoded_confirmed_block: UiConfirmedBlock){ //parses info in confirmed/finalized block    
     let block_time = encoded_confirmed_block.block_time.unwrap(); // unwrap time into int (assume time exists, may need to change to match to handle errors)
-    let block_time_str = DateTime::<Utc>::from_utc(chrono::NaiveDateTime::from_timestamp(block_time as i64, 0), Utc).to_rfc2822(); // convert int time to string time using chrono
+    let block_time_str = DateTime::<Utc>::from_utc(chrono::NaiveDateTime::from_timestamp(block_time as i64, 0), Utc).to_rfc2822(); // convert int time to string time using chrono ?
+    let mut transaction_json = Value::Null;
+    let mut rewards_json = Value::Null;
 
-    // jsonify block info
-    let block_info = json!({
+    //jsonify block info
+    let block_json = json!({
         "blockhash": encoded_confirmed_block.blockhash,
         "prev_blockhash": encoded_confirmed_block.previous_blockhash,
         "parent slot": encoded_confirmed_block.parent_slot,
-        "block height": encoded_confirmed_block.block_height,
-        "block time(str)": block_time_str,
-        "block time(int)": block_time
+        "block_height": encoded_confirmed_block.block_height,
+        "block_time(str)": block_time_str,
+        "block_time(int)": block_time,
     });
-    //print,(store to database later)
-    println!("Parsed block: {}", block_info);
 
     //check for/get transactions
     if let Some(transactions) = encoded_confirmed_block.transactions{
-        parse_transactions(transactions);
+        transaction_json = parse_transactions(transactions);
     }
     else{
         println!("No transactions in this block.");
@@ -80,15 +92,27 @@ fn parse_block(encoded_confirmed_block: UiConfirmedBlock){ //parses info in conf
 
     //check for/get rewards
     if let Some(rewards) = encoded_confirmed_block.rewards{
-        parse_rewards(rewards);
+        rewards_json = parse_rewards(rewards);
     }
     else{
         println!("No rewards in this block.");
     }
+
+    //combine into full block info JSON -- THIS IS WHAT WILL BE STORED IN AWS
+    let full_json = json!({
+        "BLOCK_INFO": block_json,
+        "TRANSACTIONS_INFO": transaction_json,
+        "REWARDS_INFO": rewards_json
+    });
+    //write JSON data to temporary file for testing
+    let file_path = "json/temp.json";
+    let mut file = File::create(file_path).expect("Failed to create file");   
+    serde_json::to_writer(&mut file, &full_json).expect("Failed to write JSON to file");
+    println!("JSON data written to {}", file_path);
 }
 
 async fn listen_to_slots() {
-    let rpc_url = "https://api.mainnet-beta.solana.com".to_string(); // testing mainnet
+    let rpc_url = "https://api.mainnet-beta.solana.com".to_string(); //mainnet
     let rpc_client = RpcClient::new(rpc_url); //connect to rpc endpoint
     let mut iter = 0;
     let config = RpcBlockConfig { //set up config to retrieve blocks
@@ -105,12 +129,12 @@ async fn listen_to_slots() {
         match latest_slot{
             Ok(slot) => {
                 println!("\nLatest Slot: {}", slot);
-                
                 let latest_block = rpc_client.get_block_with_config(slot, config); //get block at latest slot
                 loop{
                     match latest_block{
                         Ok(encoded_confirmed_block) =>{ //parse block info
                             println!("Block at slot {} found and information retrieved. Parsing...",slot);
+                            //TODO spawn new thread for every parse_block call to handle them individually
                             parse_block(encoded_confirmed_block);
                             break;
                         }
